0a300247dd31266338eaca2492fef2c493629ef7
Aaus <76867338+AausMak@users.noreply.github.com>
2022-06-26 11:08:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDUd1Mw_UzhKRIr8kr4poIChFywn_mBC-E",
+    authDomain: "spectagram-bc204.firebaseapp.com",
+    databaseURL: "https://spectagram-bc204-default-rtdb.firebaseio.com",
+    projectId: "spectagram-bc204",
+    storageBucket: "spectagram-bc204.appspot.com",
+    messagingSenderId: "1087343644353",
+    appId: "1:1087343644353:web:158ba689fce77e64a08e40"
+  };
